09887f3dce83c2d256460cc5f2f13b6c2a3791c9
Guru Prasath Anandapadmanaban <guru@xchaintechnologies.com>
2018-02-21 09:51:47 +0530

    Added more server test cases for lds-input

--- components/lds-input/test/test.server.js
+++ components/lds-input/test/test.server.js
@@ -1,11 +1,24 @@
 /* global test */
-const {expect} = require('chai');
+const {expect, assert} = require('chai');
 
-test('lds-input Basic', function (context) {
-  const output = context.render({
-    label: "First Name"
-  });
+test('Blank Check', function (context) {
+  const output = context.render({});
   
-  expect(output.$('label','.slds-form-element').text()).to.be.equal("First Name");
+  expect(output.$('div.slds-form-element').children().length).to.be.equal(2);
+  expect(output.$('div.slds-form-element').children('div').length).to.be.equal(1);
+  expect(output.$('div.slds-form-element').children('div').hasClass('slds-form-element__control')).to.be.equal(true);
+  expect(output.$('div.slds-form-element').children('label').length).to.be.equal(1);
+  expect(output.$('div.slds-form-element').children('label').hasClass('slds-form-element__label')).to.be.equal(true);
+  expect(output.$('div.slds-form-element').children('label').attr('for')).to.be.not.null;
+  expect(output.$('div.slds-form-element').children('label').text()).to.be.equal('');
 
+  expect(output.$('div.slds-form-element').children('div').children().length).to.be.equal(1);
+  expect(output.$('div.slds-form-element').children('div').children('input').length).to.be.equal(1);
+  expect(output.$('div.slds-form-element').children('div').children('input').hasClass('slds-input')).to.be.equal(true);
+  expect(output.$('div.slds-form-element').children('div').children('input').attr('type')).to.be.equal('text');
+  expect(output.$('div.slds-form-element').children('div').children('input').attr('placeholder')).to.be.equal('');
+  expect(output.$('div.slds-form-element').children('div').children('input').attr('id')).to.be.not.null;
+  const forValue = output.$('div.slds-form-element').children('label').attr('for');
+  expect(output.$('div.slds-form-element').children('div').children('input').attr('id')).to.be.equal(forValue);
+  expect(output.$('div.slds-form-element').children('div').children('input').val()).to.be.equal('');
 });
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var http = require('http')
+var http = require('http');
 
-var PORT = 1337
+var PORT = 1337;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Hello World!')
-}).listen(PORT, '127.0.0.1')
-console.log('Server running on port: ' + PORT)
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(PORT, '127.0.0.1');
+console.log('Server running on port: ' + PORT);
